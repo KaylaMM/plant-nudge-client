3810224b540039f31c3058e92247b1274d5f6505
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-08-11 23:41:27 -0400

    infrastructure complete, moving onto route building

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app, db) {
   app.post("/user", (req, res) => {
+    //  create the user here
     console.log(req.body);
     res.send("Hello");
   });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
+const db = require("./config/db");
 
 const app = express();
 
@@ -8,8 +9,11 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./app/routes")(app, {});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  require("./app/routes")(app, database);
 
-app.listen(port, () => {
-  console.log("We are live on " + port);
+  app.listen(port, () => {
+    console.log("We are live on " + port);
+  });
 });
