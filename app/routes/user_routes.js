9ca7728b6248c0843b01fbb005b22d2981531770
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-08-12 23:54:46 -0400

    first route created, error on postman :(

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,7 +1,11 @@
 module.exports = function (app, db) {
   app.post("/user", (req, res) => {
-    //  create the user here
-    console.log(req.body);
-    res.send("Hello");
+    const user = { username: req.body.username, password: req.body.password };
+    db.collections("user").insert(user, (err, result) => {});
+    if (err) {
+      res.send({ error: "An error has occurd" });
+    } else {
+      res, send(result.ops[0]);
+    }
   });
 };
