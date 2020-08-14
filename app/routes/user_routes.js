4028b0edb6de87ad3516c59c2a0b9c5b67a3bf22
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-08-13 23:50:44 -0400

    create, read, and update complete on api

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,11 +1,28 @@
+const ObjectID = require("mongodb").ObjectID;
+
 module.exports = function (app, db) {
+  //READ
+  app.get("/user/:id", (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+    db.collection("user").findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: "An error has occurd" });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  //CREATE
   app.post("/user", (req, res) => {
     const user = { username: req.body.username, password: req.body.password };
-    db.collections("user").insert(user, (err, result) => {});
-    if (err) {
-      res.send({ error: "An error has occurd" });
-    } else {
-      res, send(result.ops[0]);
-    }
+    db.collection("user").insert(user, (err, result) => {
+      if (err) {
+        res.send({ error: "An error has occurd" });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
