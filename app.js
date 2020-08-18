1ac187869d1c98b4e14c9b8bb4f1a179cefb417a
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-08-17 23:14:20 -0400

    update to user Schema an app.js. All previous middleware and framework setup

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
+
+const app = express();
+
+require("./config/db.config");
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_POINT],
+    credentials: true,
+  })
+);
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//make sure express session is used before the passport
+require("./config/session.config.js")(app);
+
+require("./config/passport/passport.config.js")(app);
+
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// default value for title local
+app.locals.title = "Express - Generated with IronGenerator";
+
+//Route SetUp
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/userPlants", require("./routes/plant"));
+//not sure if this route is needed
+app.use("/user", require("./routes/user"));
+app.use("/progressPhotos", require("./routes/progress"));
+
+module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,34 +1,3 @@
-// const mongoose = require("mongoose");
-// const { Schema, model } = mongoose;
-// const Plant = require("./Plant ");
-
-const { Schema, model } = require("mongoose");
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required."],
-    unique: true,
-    trim: true,
-  },
-  passwordHash: {
-    type: String,
-    required: [true, "Password is required."],
-    trim: true,
-  },
-  phoneNumber: {
-    type: Number,
-    required: [true, "Phone number is required."],
-    trim: true,
-  },
-  avatar: {
-    type: String,
-    default:
-      "https://i.pinimg.com/originals/55/2c/c0/552cc033eca6ec4b289a96777e078954.jpg",
-  },// const mongoose = require("mongoose");
-// const { Schema, model } = mongoose;
-// const Plant = require("./Plant ");
-
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
@@ -39,15 +8,6 @@ const userSchema = new Schema({
     unique: true,
     trim: true,
   },
-  // Email provided by users during signup
-  email: {
-    type: String,
-    required: [true, "Email is required."],
-    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
   // Password provided by users during signup
   passwordHash: {
     type: String,
@@ -55,6 +15,7 @@ const userSchema = new Schema({
     trim: true,
   },
   // Phonenumber provided by users during signup
+  //-=-=-=-=-=-=-=-=-ISSUES WITH PHONE NUMBER REQUIERD
   phoneNumber: {
     type: Number,
     required: [true, "Phone number is required."],
@@ -66,10 +27,6 @@ const userSchema = new Schema({
     default:
       "https://i.pinimg.com/originals/55/2c/c0/552cc033eca6ec4b289a96777e078954.jpg",
   },
-});
-
-module.exports = model("User", userSchema);
-
 });
 
 module.exports = model("User", userSchema);
